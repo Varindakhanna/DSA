6b77cca6215f85abb550c6476a6d032cfde7b6e3
VARINDA KHANNA <varindakhanna2002@gmail.com>
2022-06-23 14:42:33 +0530

    Arrays started

---
+++ Arrays/Reverse_array.java
@@ -0,0 +1,30 @@
+package Arrays;
+import java.io.*;
+import java.util.Arrays;
+
+public class Reverse_array {
+
+    public static void main(String[] args) 
+    {
+        int a[]={10,20,30,40};
+        reverse(a);
+        System.out.println(Arrays.toString(a));
+
+        
+    }
+
+    static void reverse(int a[])
+    {
+        int i;int temp;
+        int n=a.length;
+        System.out.println(n);
+        for(i=0;i<n/2;i++)
+        {
+            temp=a[i];
+            a[i]=a[(n-1)-i];
+            a[(n-1)-i]=temp;
+
+        }
+    }
+    
+}
