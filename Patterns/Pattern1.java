9263f5316ae1a56d70ad80b8299a7f1d697f6bd5
VARINDA KHANNA <varindakhanna2002@gmail.com>
2022-07-11 09:12:45 +0530

    Patterns

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,24 @@
+package Patterns;
+
+public class Pattern1{
+
+    public static void main(String[] args) 
+    {
+        int i,j;
+        int n=3;
+        for(i=1;i<=n;i++)
+        {
+            for ( j = 1; j <= n; j++) 
+            {
+                System.out.print(i);
+
+                
+            }
+            System.out.println();
+
+        }
+        
+    }
+
+
+}
