1b512669eca14d55b0793e2079b20137b5daf298
VARINDA KHANNA <varindakhanna2002@gmail.com>
2022-08-08 15:35:35 +0530

    dp

--- dynamic_programming/fibonacci.java
+++ dynamic_programming/fibonacci.java
@@ -5,7 +5,7 @@ public class fibonacci
     static long dp[];
     public static void main(String[] args) 
     {
-        int n=50;
+        int n=49;
         dp = new long[n+1];
         Arrays.fill(dp,-1);
         System.out.println(fibo(n));
@@ -14,27 +14,42 @@ public class fibonacci
 
     static long fibo(int n)
     {
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
+        //top down approach
 
-        else
-        {
-            if(n==1)
-            {
-            return dp[n]=0;
-            }
-            else if(n==2)
-            {
-                return dp[n]=1;
-            }
-            else
-            {
-                return dp[n]=fibo(n-1)+fibo(n-2);
-            }
+        // if(dp[n]!=-1)
+        // {
+        //     return dp[n];
+        // }
 
+        // else
+        // {
+        //     if(n==1)
+        //     {
+        //     return dp[n]=0;
+        //     }
+        //     else if(n==2)
+        //     {
+        //         return dp[n]=1;
+        //     }
+        //     else
+        //     {
+        //         return dp[n]=fibo(n-1)+fibo(n-2);
+        //     }
+
+        // }
+        
+
+        //Bottom-down approach
+        dp[0]=0;
+        dp[1]=1;
+        for (int i = 2; i <=n; i++)
+         {
+            dp[i]=dp[i-1]+dp[i-2];
+            
         }
+        return dp[n];
+       
+           
         
     }
     
